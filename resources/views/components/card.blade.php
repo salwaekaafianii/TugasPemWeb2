12bfa1e1c250cc59629674510211406b14d63ca4
salwaekaafianii <salwaafiani@gmail.com>
2025-03-22 21:09:26 +0700

    [Pertemuan 3] Membuat Layout dan Component

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,41 @@
+<div class="container mt-4">
+  <div class="row flex-nowrap overflow-auto justify-content-center">
+    
+    <div class="card mx-2 d-flex flex-column text-center" style="width: 15rem; min-height: 100%;">
+      <img src="https://cdn.gramedia.com/uploads/items/img263_TOe77MD.jpg" class="card-img-top" alt="HTML & CSS" style="object-fit: contain; height: 200px;">
+      <div class="card-body d-flex flex-column">
+        <h5 class="card-title">HTML & CSS</h5>
+        <p class="card-text">Belajar dasar HTML & CSS.</p>
+        <a href="#" class="btn btn-danger mt-auto">Lihat Buku</a>
+      </div>
+    </div>
+
+    <div class="card mx-2 d-flex flex-column text-center" style="width: 15rem; min-height: 100%;">
+      <img src="https://image.gramedia.net/rs:fit:0:0/plain/https://cdn.gramedia.com/uploads/items/9786230005633_Cov_Lancar_Ja.jpg" class="card-img-top" alt="JavaScript" style="object-fit: contain; height: 200px;">
+      <div class="card-body d-flex flex-column">
+        <h5 class="card-title">JavaScript</h5>
+        <p class="card-text">Pemrograman web interaktif.</p>
+        <a href="#" class="btn btn-danger mt-auto">Lihat Buku</a>
+      </div>
+    </div>
+
+    <div class="card mx-2 d-flex flex-column text-center" style="width: 15rem; min-height: 100%;">
+      <img src="https://cdn.gramedia.com/uploads/product-metas/s8atcazs-c.jpg" class="card-img-top" alt="Data Science" style="object-fit: contain; height: 200px;">
+      <div class="card-body d-flex flex-column">
+        <h5 class="card-title">Data Science</h5>
+        <p class="card-text">Dasar-dasar Data Science.</p>
+        <a href="#" class="btn btn-danger mt-auto">Lihat Buku</a>
+      </div>
+    </div>
+
+    <div class="card mx-2 d-flex flex-column text-center" style="width: 15rem; min-height: 100%;">
+      <img src="https://image.gramedia.net/rs:fit:0:0/plain/https://cdn.gramedia.com/uploads/items/9786020497082_Network_Tweaking-dan-Hacking.jpg" class="card-img-top" alt="Networking" style="object-fit: contain; height: 200px;">
+      <div class="card-body d-flex flex-column">
+        <h5 class="card-title">Networking</h5>
+        <p class="card-text">Jaringan komputer dasar.</p>
+        <a href="#" class="btn btn-danger mt-auto">Lihat Buku</a>
+      </div>
+    </div>
+
+  </div>
+</div>
---
+++ resources/views/web/homepage.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <h4 class="text-center">Jelajahi Dunia Ilmu dengan Buku Terbaik!</h4>
+    <x-card></x-card>
+    <br>
+    <x-alert></x-alert>
+</x-layout>
